a1793b8ba5eb262ca2b88cdbee0e916dfbd79c28
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-06-17 01:53:13 -0400

    Add RPC contract call debug logs

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -2186,6 +2186,7 @@ namespace cryptonote
   //------------------------------------------------------------------------------------------------------------------------------
   bool core_rpc_server::on_call_contract(const COMMAND_RPC_CALL_CONTRACT::request& req, COMMAND_RPC_CALL_CONTRACT::response& res)
   {
+    MDEBUG("on_call_contract account:" << req.account << " caller:" << req.caller << " write:" << std::boolalpha << req.write << " fee:" << req.fee << " data:" << req.data);
     if (boost::algorithm::starts_with(req.data, "deposit:"))
     {
       if (!req.write)
@@ -2249,6 +2250,7 @@ namespace cryptonote
       std::string bin;
       if (!epee::string_tools::parse_hexstr_to_binbuff(req.data, bin))
       {
+        MERROR("on_call_contract: failed to parse hex data " << req.data);
         res.status = CORE_RPC_STATUS_FAILED;
         return false;
       }
