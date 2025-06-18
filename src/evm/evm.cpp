6ee999077af631e3582143c66abe68b05edfa1ca
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-06-18 12:15:48 -0400

    Implement missing EVM opcodes

--- src/evm/evm.cpp
+++ src/evm/evm.cpp
@@ -515,6 +515,9 @@ int64_t EVM::execute(const std::string& self, Contract& contract, const std::vec
           }
         }
         break;
+      case 0x40: { // BLOCKHASH
+        stack.push_back(0);
+        break;
       }
       case 0x42: { // TIMESTAMP
         stack.push_back(timestamp);
@@ -522,6 +525,10 @@ int64_t EVM::execute(const std::string& self, Contract& contract, const std::vec
       }
       case 0x43: { // NUMBER
         stack.push_back(block_height);
+        break;
+      }
+      case 0x48: { // BASEFEE
+        stack.push_back(0);
         break;
       }
       case 0x50: { // POP
@@ -552,6 +559,10 @@ int64_t EVM::execute(const std::string& self, Contract& contract, const std::vec
       }
       case 0x58: { // PC
         stack.push_back(pc - 1);
+        break;
+      }
+      case 0x59: { // MSIZE
+        stack.push_back(memory.size());
         break;
       }
       case 0x5a: { // GAS
