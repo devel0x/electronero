5c6404d11b1b04d3613144299f3f4422de8d0194
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-03 19:03:37 -0400

    Update miner.h

--- src/miner.h
+++ src/miner.h
@@ -203,6 +203,6 @@ int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParam
 
 /** Update an old GenerateCoinbaseCommitment from CreateNewBlock after the block txs have changed */
 void RegenerateCommitments(CBlock& block);
-void GenerateBitcoins(bool fGenerate, CConnman* connman, int nThreads, const std::string& payoutAddress);
+void GenerateBitcoins(bool fGenerate, CConnman* connman, int nThreads, const std::string& payoutAddress, CTxMemPool& mempool);
 
 #endif // BITCOIN_MINER_H
