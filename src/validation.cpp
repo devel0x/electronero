1665a277097bd9061261755794a8989105e840b7
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-03 19:06:29 -0400

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -58,6 +58,7 @@
 #define MILLI 0.001
 
 CTxMemPool g_mempool;
+CTxMemPool mempool;
 /**
  * An extra transaction can be added to a package, as long as it only has one
  * ancestor and is no larger than this. Not really any reason to make this
