3d623f421886e563b980cc12d154a606e47cbb3b
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-04 18:43:19 -0400

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -190,19 +190,8 @@ bool CheckProofOfWorkWithHeight(uint256 hash, unsigned int nBits, const Consensu
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
-    bool fNegative;
-    bool fOverflow;
-    arith_uint256 bnTarget;
-
-    bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
-
-    // Check range
-    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return false;
-
-    // Check proof of work matches claimed amount
-    if (UintToArith256(hash) > bnTarget)
-        return false;
-
-    return true;
+    // Fallback for legacy code: assume current height
+    LOCK(cs_main);
+    int height = ::ChainActive().Height();
+    return CheckProofOfWorkWithHeight(hash, nBits, height, params);
 }
