a06a66bda3d06970413d3b68bb0f8f14351712c8
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-05 10:38:43 -0400

    Update blockfilter.cpp

--- src/test/util/blockfilter.cpp
+++ src/test/util/blockfilter.cpp
@@ -11,7 +11,7 @@
 bool ComputeFilter(BlockFilterType filter_type, const CBlockIndex* block_index, BlockFilter& filter)
 {
     CBlock block;
-    if (!ReadBlockFromDisk(block, block_index->GetBlockPos(), Params().GetConsensus())) {
+    if (!ReadBlockFromDisk(block, block_index->GetBlockPos(), Params().GetConsensus(), block_index->nHeight)) {
         return false;
     }
 
