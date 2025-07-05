89bd743e682c909eb629dab374ebc38ff5699b27
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-04 21:26:00 -0400

    YESPOWER

--- src/pow/yespower.cpp
+++ src/pow/yespower.cpp
@@ -6,20 +6,30 @@
 #include "pow.h"
 #include "pow/yespower.h"
 #include "crypto/yespower/yespower.h"
+#include "hash.h"
+
+static const yespower_params_t yespower_version_1_0 = {
+    .version = YESPOWER_1_0,
+    .N = 2048,
+    .r = 8,
+    .pers = NULL,
+    .perslen = 0
+};
 
 uint256 YespowerHash(const CBlockHeader& block)
 {
-    static const yespower_params_t yespower_version_1_0 = {
-        .version = YESPOWER_1_0,
-        .N = 2048,
-        .r = 8,
-        .pers = NULL,
-        .perslen = 0
-    };
-
     uint256 hash;
     if (yespower_tls((const uint8_t*)&block, sizeof(CBlockHeader), &yespower_version_1_0, (yespower_binary_t*)&hash) != 0)
         abort();
 
     return hash;
+}
+
+bool CheckYespower(const CBlockHeader& block, const arith_uint256& bnTarget)
+{
+    uint256 hash;
+    if (yespower_tls((const uint8_t*)&block, sizeof(CBlockHeader), &yespower_version_1_0, (yespower_binary_t*)&hash) != 0)
+        return false;
+
+    return UintToArith256(hash) <= bnTarget;
 }
