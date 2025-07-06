a52cf1453eb6ada3b35099914baad30d6c13b896
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-06 14:00:06 -0400

    Update yespower.h

--- src/pow/yespower.h
+++ src/pow/yespower.h
@@ -5,6 +5,7 @@
 #include "uint256.h"
 #include "arith_uint256.h"
 #include "primitives/block.h"
+#include "crypto/yespower/yespower.h"
 
 uint256 YespowerHash(const CBlockHeader& block, yespower_local_t* shared, int height);
 bool CheckYespower(const CBlockHeader& block, const arith_uint256& bnTarget, int height);
