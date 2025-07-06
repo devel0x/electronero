4bfcfc1380c2c18cf856b20371e4852650dba487
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-06 17:28:32 -0400

    Update yespower.cpp

--- src/pow/yespower.cpp
+++ src/pow/yespower.cpp
@@ -20,8 +20,8 @@ static const yespower_params_t yespower_default = {
 // Interchained optimized (post-fork)
 static const yespower_params_t yespower_interchained = {
     .version = YESPOWER_1_0,
-    .N = 2048,
-    .r = 8,
+    .N = 1024,
+    .r = 4,
     .pers = NULL,
     .perslen = 0
 };
@@ -45,7 +45,7 @@ uint256 YespowerHash(const CBlockHeader& block, yespower_local_t* shared, int he
 {
     uint256 hash;
     const Consensus::Params& params = Params().GetConsensus();
-    const yespower_params_t* algo = (height >= 1)
+    const yespower_params_t* algo = (height >= 2)
         ? &yespower_interchained
         : &yespower_default;
 
@@ -61,7 +61,7 @@ bool CheckYespower(const CBlockHeader& block, const arith_uint256& bnTarget, int
 {
     uint256 hash;
     const Consensus::Params& params = Params().GetConsensus();
-    const yespower_params_t* algo = (height >= 1)
+    const yespower_params_t* algo = (height >= 2)
         ? &yespower_interchained
         : &yespower_default;
 
