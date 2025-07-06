58983430cb585b560ce08e1ad9d0c52f332e206c
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-06 13:46:40 -0400

    Update yespower.h

--- src/pow/yespower.h
+++ src/pow/yespower.h
@@ -6,7 +6,7 @@
 #include "arith_uint256.h"
 #include "primitives/block.h"
 
-uint256 YespowerHash(const CBlockHeader& block, );
+uint256 YespowerHash(const CBlockHeader& block, yespower_local_t* shared, int height);
 bool CheckYespower(const CBlockHeader& block, const arith_uint256& bnTarget, int height);
 
 #endif // INTERCHAINED_YESPOWER_H
