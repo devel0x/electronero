1c85fa5f812276574b6180d136e20e242d248ddc
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-07 20:59:18 -0400

    Update blockencodings.cpp

--- src/blockencodings.cpp
+++ src/blockencodings.cpp
@@ -218,4 +218,13 @@ ReadStatus PartiallyDownloadedBlock::FillBlock(CBlock& block, const std::vector<
             return READ_STATUS_FAILED;
         return READ_STATUS_CHECKBLOCK_FAILED;
     }
+        
+    LogPrint(BCLog::CMPCTBLOCK, "Successfully reconstructed block %s with %lu txn prefilled, %lu txn from mempool (incl at least %lu from extra pool) and %lu txn requested\n", hash.ToString(), prefilled_count, mempool_count, extra_count, vtx_missing.size());
+    if (vtx_missing.size() < 5) {
+        for (const auto& tx : vtx_missing) {
+            LogPrint(BCLog::CMPCTBLOCK, "Reconstructed block %s required tx %s\n", hash.ToString(), tx->GetHash().ToString());
+        }
+    }
+
+    return READ_STATUS_OK;
 }
