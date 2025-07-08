9be9fee2f4d246655d72b2a8d34624925e32d9c1
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-07 22:26:33 -0400

    Update mining.cpp

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -728,8 +728,12 @@ static RPCHelpMan getblocktemplate()
     if(!node.connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    if (node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
+    int inPeers = node.connman->GetNodeCount(CConnman::CONNECTIONS_IN);
+    int outPeers = node.connman->GetNodeCount(CConnman::CONNECTIONS_OUT);
+    
+    if (inPeers == 0 && outPeers == 0) {
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, PACKAGE_NAME " is not connected!");
+    }
 
     if (::ChainstateActive().IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, PACKAGE_NAME " is in initial sync and waiting for blocks...");
