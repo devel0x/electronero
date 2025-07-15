ae75985ac691393701828a4b45eb7ea1c6b6aeff
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-15 18:57:37 -0400

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -53,6 +53,7 @@ struct Params {
     int kawpowForkHeight;
     int difficultyForkHeight;
     int nextDifficultyForkHeight;
+    uint256 powLimitEasyBlocks;
     /* Block hash that is excepted from BIP16 enforcement */
     uint256 BIP16Exception;
     /** Block height and hash at which BIP34 becomes active */
