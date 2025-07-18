507357eecd1c516812617605a13e7e6749b628c8
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-18 19:49:07 -0400

    Update rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4919,7 +4919,6 @@ static RPCHelpMan tokenapprove()
             op.timestamp = GetTime();
             op.signer = signer;
             op.wallet_name = walletName;
-            op.memo = memo;
 
             if (!g_token_ledger.SignTokenOperation(op, *wallet, walletName, witness)) {
                 throw JSONRPCError(RPC_WALLET_ERROR, "Signing failed");
