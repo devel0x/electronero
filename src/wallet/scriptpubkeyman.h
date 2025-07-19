6928310615854a06a7eedab87f6222568a0ddbf8
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-07-18 21:39:39 -0400

    Update scriptpubkeyman.h

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -233,7 +233,7 @@ public:
     virtual bool SignTransaction(CMutableTransaction& tx, const std::map<COutPoint, Coin>& coins, int sighash, std::map<int, std::string>& input_errors) const { return false; }
     /** Sign a message with the given script */
     virtual SigningResult SignMessage(const std::string& message, const PKHash& pkhash, std::string& str_sig) const { return SigningResult::SIGNING_FAILED; };
-    virtual SigningResult SignMessage(const std::string& message, const WitnessV0KeyHash& wpkh, std::string& str_sig) const override;
+    virtual SigningResult SignMessage(const std::string& message, const WitnessV0KeyHash& wpkh, std::string& str_sig) const;
 
     /** Adds script and derivation path information to a PSBT, and optionally signs it. */
     virtual TransactionError FillPSBT(PartiallySignedTransaction& psbt, int sighash_type = 1 /* SIGHASH_ALL */, bool sign = true, bool bip32derivs = false, int* n_signed = nullptr) const { return TransactionError::INVALID_PSBT; }
