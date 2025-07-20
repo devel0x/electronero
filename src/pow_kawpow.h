3059bfc66856507bfe964e987a61d0ffc5385e02
Interchained <president@worldvaporexpo.com>
2025-07-20 19:03:05 -0400

    checkpoint: revert unecessary commits and fixup some pow stuff

--- src/pow_kawpow.h
+++
@@ -1,9 +0,0 @@
-#ifndef BITCOIN_POW_KAWPOW_H
-#define BITCOIN_POW_KAWPOW_H
-
-#include "uint256.h"
-
-// Returns the DAG seed hash for a given height
-uint256 GetKAWPOWSeed(int height);
-
-#endif // BITCOIN_POW_KAWPOW_H
