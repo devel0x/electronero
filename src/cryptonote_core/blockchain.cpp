d5f3100eb3bb720bbd039118b574c056a1687ed8
Interchained <president@worldvaporexpo.com>
2025-07-31 17:22:50 -0400

    checkpoint windows

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -34,7 +34,7 @@
 #include <cstdio>
 #include <boost/filesystem.hpp>
 #include <boost/range/adaptor/reversed.hpp>
-
+#include <boost/asio/post.hpp>
 #include "include_base_utils.h"
 #include "cryptonote_basic/cryptonote_basic_impl.h"
 #include "tx_pool.h"
